26a5a0e1bb941b6d32219ffc3bdced1ee3d67e1e
hutao <1131589588@qq.com>
2023-03-02 07:51:44 +0800

    feat: add test page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import useSWR from "swr";
+
+const apiBase = "https://cnodejs.org/api/v1"
+
+async function fetcher(apiPath) {
+  const res = await axios.get(`${apiBase}${apiPath}`)
+  // console.log(1111, res)
+  return res.data?.data || [];
+}
 
 function App() {
+  const { data, isLoading } = useSWR("/topics", fetcher)
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>测试客户端渲染</h2>
+      {isLoading ? "loading" : (
+        <ul>
+          {data.map(item => (
+            <li key={item.id}>
+              <a href={`https://cnodejs.org/topic/${item.id}`} target="_blank" rel="noreferrer" >{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
