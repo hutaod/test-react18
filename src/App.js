1cb586e3f01daf83f8271de82afb4387f2628d34
hellott <1131589588@qq.com>
2023-03-02 13:09:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
 import axios from "axios";
 import useSWR from "swr";
 
-const apiBase = "https://cnodejs.org/api/v1"
+const apiBase = "https://cnodejs.org/api/v1";
 
 async function fetcher(apiPath) {
-  const res = await axios.get(`${apiBase}${apiPath}`)
-  // console.log(1111, res)
+  const res = await axios.get(`${apiBase}${apiPath}`);
   return res.data?.data || [];
 }
 
 function App() {
-  const { data, isLoading } = useSWR("/topics", fetcher)
-  console.log(data)
+  const { data, isLoading } = useSWR("/topics", fetcher);
   return (
     <div className="App">
       <h2>测试客户端渲染</h2>
-      {isLoading ? "loading" : (
+      {isLoading ? (
+        "loading"
+      ) : (
         <ul>
-          {data.map(item => (
+          {data.map((item) => (
             <li key={item.id}>
-              <a href={`https://cnodejs.org/topic/${item.id}`} target="_blank" rel="noreferrer" >{item.title}</a>
+              <a
+                href={`https://cnodejs.org/topic/${item.id}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {item.title}
+              </a>
             </li>
           ))}
         </ul>
